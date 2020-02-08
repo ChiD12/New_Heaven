2c9d116ccb465ea85bba0c97a95b6ffb3f9bf38e
ChiD12 <daniel.chid@gmail.com>
2020-02-07 19:21:43 -0500

    edgelist

--- GBMap.cpp
+++ GBMap.cpp
@@ -3,38 +3,93 @@
 #include <iostream>
 using namespace std;
 
-void main() {
+int main() {
 	GameBoard* gb = new GameBoard(14, 14);
+    //int count = 0;
+    // for (size_t i = 0; i < gb->gbA.size(); i++)
+	// {
+	// 	for (size_t j = 0; j < gb->gbA[i].size(); j++) {
+	// 		//gb->gbA[i][j] = new Node(count);
+    //         //cout << gb->gbA[i][j]->count;
+    //         //cout << " ";
+	// 	}
+    //     cout << endl;
+	// }
+    Node* current = gb->gbA[3][3];
+    for (size_t i = 0; i < 13; i++){
+        
+        cout << *(current->x_index) << "  "  << *(current->y_index) << endl;
+        //cout << (current->edge_list[2])<< endl;
+        current = (current->edge_list[2]);
+        
+    }
+    
 }
 
 
 Node::Node() {};
-Node::Node(int* x, int* y) {
-	x_index = x;
-	y_index = y;
+Node::Node(int x, int y) {
+	x_index = new int(x);
+	y_index = new int(y);
 	type = EMPTY;
-	edge_list = new Node[4]; // 0 is north, 1 is east, 2 is south, 3 is west
+    edge_list =vector<Node*>(4);
+    //Node *arr[4];
+	//edge_list = arr; // 0 is north, 1 is east, 2 is south, 3 is west
 }
+Node::Node(int c){
+    count = c;
+};
 
-void Node::update_edge()
-{
+void GameBoard::update_edge_all(){
+
+    for (size_t i = 0; i < gbA.size(); i++){
+        for (size_t j = 0; j < gbA[i].size(); j++){
+            for (size_t k = 0; k < 4; k++){
+                if(gbA[i][j]!= NULL){  //if current node is not null 
+                    if(k == 0 && j != 0 && gbA[i][j-1] != NULL){ //north
+                        gbA[i][j]->edge_list[k] = (gbA[i][j-1]);
+                    }
+                    if(k == 1 && i != 13 && gbA[i+1][j] != NULL){ //east
+                        gbA[i][j]->edge_list[k] = (gbA[i+1][j]);
+                    }
+                    if(k == 2 && j != 0 && gbA[i][j+1] != NULL){ //south
+                        gbA[i][j]->edge_list[k] = (gbA[i][j+1]);
+                    }
+                    if(k == 3 && i != 13 && gbA[i-1][j] != NULL){ //west
+                        gbA[i][j]->edge_list[k] = (gbA[i-1][j]);
+                    }
+                }
+            }
+        }
+    }
 }
 
 
 GameBoard::GameBoard() {};
 GameBoard::GameBoard(const int x, const int y) {
-	this->x = &x;
-	this->y = &y;
+	xSize = &x;
+	ySize = &y;
 
-	gb = vector<vector<Node>>(*this->x, vector<Node>(*this->y));
+	gbA = vector<vector<Node*>>((*xSize), vector<Node*>((*ySize)));
 
 	int count = 0;
-	for (size_t i = 0; i < gb.size(); i++)
-	{
-		for (size_t j = 0; j < gb[i].size(); j++) {
-			count++;
+	for (size_t i = 0; i < gbA.size(); i++){
+		for (size_t j = 0; j < gbA[i].size(); j++){
+            
+			if(!((*xSize) == 14 && (*ySize) == 14 && (i == 0 && j == 0) || (i == 0 && j == 13) || (i == 13 && j == 0 ) || (i == 13 && j == 13))){
+                gbA[i][j] = new Node(i,j);
+            }
 		}
 	}
-
-	cout << count << endl;
-}
+    update_edge_all();
+    
+    
+    for (size_t i = 0; i < gbA.size(); i++){
+		for (size_t j = 0; j < gbA[i].size(); j++){
+                if(gbA[i][j] != NULL){
+                    //cout << *(gbA[i][j]->y_index) << endl;
+                    //cout << "(x: "<<*(gbA[i][j]->x_index) <<" y: "<<*(gbA[i][j]->y_index)<<") ";
+                }
+		}//cout << endl;
+	}
+};
--- GBMap.hpp
+++ GBMap.hpp
@@ -3,26 +3,35 @@ using namespace std;
 
 enum resource_type { WOOD, STONE, SHEEP, GRAIN, EMPTY };
 
+
 class Node {
 public:
 	int* x_index;
 	int* y_index;
 
+	//bool* invalid = new bool(false);
+	 
+	int count =0;
+
 	resource_type type;
 	resource_type* p_type;
 
-	Node* edge_list;
+	vector<Node*> edge_list;
 	Node();
-	Node(int* x, int* y);
+	Node(int x, int y);
+	Node(int c);
 
-	void update_edge();
+	
 };
 
 class GameBoard {
 public:
-	const int* x;
-	const int* y;
-	vector<vector<Node>> gb;
+	const int* xSize;
+	const int* ySize;
+	vector<vector<Node*>> gbA;
+
+	void update_edge(int x, int y);
+	void update_edge_all();
 
 	GameBoard();
 	GameBoard(const int x, const int y);
